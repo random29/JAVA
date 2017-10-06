154a449fdb5b6f33ce7844cf520fb0412cef9336
Ilya Ilyin <32275636+random28@users.noreply.github.com>
2017-10-06 20:48:46 +0300

    Update DocumentUtil.java

--- Homeworks/Home5/src/ru/itpark/DocumentUtil.java
+++ Homeworks/Home5/src/ru/itpark/DocumentUtil.java
@@ -3,15 +3,17 @@ package com.company;
 import java.util.Scanner;
 
 public class DocumentUtil {
-    static int count =0;
+    static int count = 0;
     String lines[] = new String[6];
-    public void showdocument(){
+
+    public void showdocument() {
         for (int i = 0; i < lines.length; i++) {
             String lineToOut = lines[i] == null ? "_______" : lines[i];
             System.out.println(i + ": " + lineToOut);
         }
     }
-    public void inbegin(String newLine){
+
+    public void inbegin(String newLine) {
 
         for (int i = count; i > 0; i--) {
             lines[i] = lines[i - 1];
@@ -19,7 +21,8 @@ public class DocumentUtil {
         lines[0] = newLine;
         count++;
     }
-    public void inend(String newLine){
+
+    public void inend(String newLine) {
         if (count == lines.length) {
             System.err.println("Нет места");
             return;
@@ -27,25 +30,33 @@ public class DocumentUtil {
         lines[count] = newLine;
         count++;
     }
-    public void deletestring(int newLine){
+
+    public void deletestring(int newLine) {
         if (newLine == count) {
-            lines[newLine]=null;
+            lines[newLine] = null;
             count--;
-        }
-        else
-        {
-            for (int i=newLine; i<count ; i++) {
-                lines[i]=lines[i+1];
+        } else {
+            for (int i = newLine; i < count; i++) {
+                lines[i] = lines[i + 1];
             }
-            lines[count]=null;
+            lines[count] = null;
             count--;
         }
-    }
-    public void cleanme(int index){
-        lines[index]=null;
     }
 
-    public void changeme(int index,String newLine){
-        lines[index]=newLine;
+    public void cleanme(int index) {
+        lines[index] = null;
+    }
+
+    public void changeme(int index, String newLine) {
+        lines[index] = newLine;
+    }
+
+    public static void printall(DocumentUtil document[], int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.println(i + " :");
+            document[i].showdocument();
+            System.out.println();
+        }
     }
 }
